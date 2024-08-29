5d7f9992a4928794bb36b80a1d2a2570e0b89836
axyut <aoogleak@gmail.com>
2024-08-29 14:07:41 +0545

    init: payment esewa desc

--- packages/Webkul/Payment/src/Payment/Esewa.php
+++ packages/Webkul/Payment/src/Payment/Esewa.php
@@ -18,6 +18,8 @@ class Esewa extends Payment
      *
      * @return string
      */
+
+    // here we will return the url of the esewa payment gateway
     public function getRedirectUrl() {}
 
     /**
@@ -31,8 +33,14 @@ class Esewa extends Payment
             $this->setCart();
         }
 
+        // here we will check if the payment method is active and the cart has stockable items
         return $this->getConfigData('active') && $this->cart?->haveStockableItems();
     }
+
+    // need to check on details like 
+    // make that units of the product lock when the user clicks on the payment button
+    // make sure the user can't change/remove the quantity of the product after clicking on the payment button
+    // redirection after payment from esewa to the website will send the user to order complete/decline page
 
     /**
      * Get payment method image.
